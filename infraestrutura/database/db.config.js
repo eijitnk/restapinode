c8b154ee997b6ac43822b0a4bf14462c52a7d6d9
Willian Eiji Tanaka <willianeijitanaka@gmail.com>
2026-01-28 23:26:15 -0300

    AlterarBanco

--- infraestrutura/database/db.config.js
+++ infraestrutura/database/db.config.js
@@ -1,7 +1,3 @@
 module.exports = {
-    HOST: 'us-cdbr-east-02.cleardb.com',
-    USER: 'ba3996c583d29c',
-    PASSWORD: 'e737274a',
-    DATABASE: 'heroku_6d8b1ede2e7f849'
-    // multipleStatements: true
-}
+    URL: process.env.JAWSDB_URL || 'mysql://usuario:senha@localhost:3306/nome_local'
+};
